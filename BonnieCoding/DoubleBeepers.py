da41ddd0030778ef3a4311027faa9b8355929481
bonnie0301 <103303293+bonnie0301@users.noreply.github.com>
2022-04-17 15:51:54 +0800

    Add files via upload

---
+++ BonnieCoding/DoubleBeepers.py
@@ -0,0 +1,37 @@
+"""
+File: DoubleBeepers.py
+Name:
+-------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    move()
+    double_beepers()
+    move_back()
+    go_back()
+
+
+def move_back()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
